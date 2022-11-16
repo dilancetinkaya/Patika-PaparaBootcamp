274f56387da084e4c08976e9907836ee1aaf3d04
dilancetinkaya <dilancetinkaya007@gmail.com>
2022-11-16 22:54:42 +0300

    add homework two

---
+++ DilanCetinkaya-Week2-Homework/Owner.API/Owner.API/Controllers/OwnerController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Owner.API.Data;
+using Owner.API.Model;
+using System.Linq;
+
+namespace Owner.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        [Route("All")]
+        [HttpGet]
+        public IActionResult GetAllOwner()
+        {
+            var owners = new OwnerData().GetAllOwner();
+            return Ok(owners);
+        }
+
+        [Route("Create")]
+        [HttpPost]
+        public IActionResult AddOwner(OwnerModel owner)
+        {
+            if (owner.Description.Contains("hack"))
+                throw new BadHttpRequestException("no contain \"hack\"");
+
+            var owners = new OwnerData().GetAllOwner();
+            owners.Add(owner);
+            return Ok(owners);
+
+        }
+        [Route("Update")]
+        [HttpPut]
+        public IActionResult UpdateOwner(int id, OwnerModel owner)
+        {
+            if (id != owner.Id)
+                throw new BadHttpRequestException("Id not match ");
+
+            var owners = new OwnerData().GetAllOwner();
+            var updateOwner = owners.FirstOrDefault(x => x.Id == id);
+            if(updateOwner == null)
+                throw new BadHttpRequestException("Id not found");
+
+            updateOwner.Name = owner.Name;
+            updateOwner.Surname = owner.Surname;
+            updateOwner.Type = owner.Type;
+            updateOwner.Date = owner.Date;
+            updateOwner.Description = owner.Description;
+            return Ok(updateOwner);
+
+        }
+        [Route("Delete")]
+        [HttpDelete]
+        public IActionResult DeleteOwner(int id)
+        {
+            var owners = new OwnerData().GetAllOwner();
+            var owner= owners.FirstOrDefault(x => x.Id == id);
+            if(owner==null)
+                throw new BadHttpRequestException("Record is not found");
+
+            owners.Remove(owner);
+            return Ok(owners);
+        }
+    }
+}
---
+++ DilanCetinkaya-Week2-Homework/Owner.API/Owner.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Owner.API.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception exception)
+            {
+                httpContext.Response.ContentType = "Application/json";
+                httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                await httpContext.Response.WriteAsync(new ErrorDetail()
+                {
+                    StatusCode = httpContext.Response.StatusCode,
+                    Message = exception.Message
+                }.ToString());
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
